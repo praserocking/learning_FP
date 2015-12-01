825393af6262d4ff5438f4423dec4e6aadc4d1c2
praserocking <praserocking@gmail.com>
2015-12-01 16:56:13 +0530

    adding TOH

--- basics.scala
+++ basics.scala
@@ -1,8 +1,13 @@
-object sample extends App{
+object basics extends App{
 	
 	def fibanocci(a:Int) : Int = if(a == 1 || a == 2) a else (fibanocci(a-1) + fibanocci(a-2))
+	
 	def factorial(a:Int) : Int = if(a == 1) a else a * factorial(a-1)
+	
+	def _toh_helper(src: String, dst: String) = println("move "+src+" to "+dst+"\n")
+	def toh(src: String, intr: String, dest: String, n:Int) : Unit = if(n == 1)_toh_helper(src,dest) else {toh(src,dest,intr,n-1);_toh_helper(src,dest);toh(intr,src,dest,n-1)}
 	
 	println(fibanocci(5))
 	println(factorial(5))
+	println(toh("A","B","C",3))
 }
